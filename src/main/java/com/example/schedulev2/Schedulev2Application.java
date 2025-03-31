9603ec6f52bd8b174d86b34eaea4addf8b7a5d5d
byeongtaek12 <byeongtaek12@naver.com>
2025-03-31 16:35:10 +0900

    Initial commit

---
+++ src/main/java/com/example/schedulev2/Schedulev2Application.java
@@ -0,0 +1,13 @@
+package com.example.schedulev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Schedulev2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Schedulev2Application.class, args);
+    }
+
+}
