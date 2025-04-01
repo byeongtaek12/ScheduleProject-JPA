27860cb36776399d580482599c414e8886d48413
obt <byeongtaek12@naver.com>
2025-04-01 18:01:31 +0900

    feat: WriterResponseDto클래스) 이름 수정 및 생성자 추가
    회원가입에서만 쓰이는 객체가 아니라 여러 군데에서 사용될 가능성이 있어서 이름 수정해줬고, 유저들 조회할 때 findAll()
    메서드를 쓰게 되면 Writer 타입의 list로 반환되게 되는데 Writer 타입을 WriterResponseDto로 바꿔주기 위해 생성자 추가했음

--- src/main/java/com/example/schedulev2/dto/WriterResponseDto.java
+++ src/main/java/com/example/schedulev2/dto/WriterResponseDto.java
@@ -1,11 +1,12 @@
 package com.example.schedulev2.dto;
 
+import com.example.schedulev2.entity.Writer;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
-public class SignUpResponseDto {
+public class WriterResponseDto {
 
     private final Long id;
 
@@ -15,10 +16,14 @@ public class SignUpResponseDto {
 
     private final LocalDateTime modifiedAt;
 
-    public SignUpResponseDto(Long id, String writer, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+    public WriterResponseDto(Long id, String writer, LocalDateTime createdAt, LocalDateTime modifiedAt) {
         this.id = id;
         this.writer = writer;
         this.createdAt = createdAt;
         this.modifiedAt = modifiedAt;
+    }
+
+    public WriterResponseDto(Writer writer) {
+        this(writer.getId(),writer.getWriter(), writer.getCreatedAt(),writer.getModifiedAt());
     }
 }
