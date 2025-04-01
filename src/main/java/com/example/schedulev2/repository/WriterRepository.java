d442518d8a3650c7e846679cfd69e1f24885d0b7
obt <byeongtaek12@naver.com>
2025-04-01 15:03:13 +0900

    feat: WriterRepository인터페이스) default 메서드 추가
    id 값을 받아서 유저 찾는 메서드 추가

--- src/main/java/com/example/schedulev2/repository/WriterRepository.java
+++ src/main/java/com/example/schedulev2/repository/WriterRepository.java
@@ -1,4 +1,16 @@
 package com.example.schedulev2.repository;
 
-public interface WriterRepository {
+import com.example.schedulev2.entity.Writer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
+
+@Repository
+public interface WriterRepository extends JpaRepository<Writer, Long> {
+
+    default Writer findByIdOrElseThrow(Long writer_id){
+        return findById(writer_id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                        "Does not exist writer_id: " + writer_id));
+    }
 }
