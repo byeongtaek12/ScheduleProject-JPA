2619fe56cb370d328bd17e8bdce06b18ce082ee1
obt <byeongtaek12@naver.com>
2025-04-01 14:48:45 +0900

    feat: CreateScheduleRequestDto클래스) 멤버 추가
    ``

--- src/main/java/com/example/schedulev2/dto/CreateScheduleRequestDto.java
+++ src/main/java/com/example/schedulev2/dto/CreateScheduleRequestDto.java
@@ -1,4 +1,14 @@
 package com.example.schedulev2.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class CreateScheduleRequestDto {
+
+    private String title;
+
+    private String contents;
+
 }
