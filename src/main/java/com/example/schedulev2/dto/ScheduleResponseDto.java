df193d67ba7db6562999a93d01f693200948df17
obt <byeongtaek12@naver.com>
2025-04-01 15:00:16 +0900

    feat: ScheduleResponseDto클래스) 멤버들 추가
    ``

--- src/main/java/com/example/schedulev2/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/schedulev2/dto/ScheduleResponseDto.java
@@ -1,4 +1,30 @@
 package com.example.schedulev2.dto;
 
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+@Getter
 public class ScheduleResponseDto {
+
+    private Long id;
+
+    private Long writer_id;
+
+    private String title;
+
+    private String contents;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime modifiedAt;
+
+    public ScheduleResponseDto(Long id, Long writer_id, String title, String contents, LocalDateTime createdAt,
+                               LocalDateTime modifiedAt) {
+        this.id=id;
+        this.writer_id = writer_id;
+        this.title = title;
+        this.contents = contents;
+        this.createdAt = createdAt;
+        this.modifiedAt = modifiedAt;
+    }
 }
