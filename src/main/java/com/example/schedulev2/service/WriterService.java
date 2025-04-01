e63139a70b43ff90c67f0e621dc5606827dc803b
obt <byeongtaek12@naver.com>
2025-04-01 15:40:49 +0900

    feat: WriterService인터페이스) 메서드 추가
    회원가입 메서드 추가

--- src/main/java/com/example/schedulev2/service/WriterService.java
+++ src/main/java/com/example/schedulev2/service/WriterService.java
@@ -1,4 +1,8 @@
 package com.example.schedulev2.service;
 
+import com.example.schedulev2.dto.SignUpResponseDto;
+
 public interface WriterService {
+    SignUpResponseDto signUp(String writer, String email, String password);
+
 }
