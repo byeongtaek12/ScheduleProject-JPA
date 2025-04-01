21e8cd7cdefe1b9902afbe584f3e6f6dde0bbb22
obt <byeongtaek12@naver.com>
2025-04-01 14:58:05 +0900

    feat: ScheduleController클래스) 컨트롤러 생성 및 일정 생성 기능 추가
    유저의 아이디값을 파라미터로 받아서 일정을 생성하는 메서드 추가

--- src/main/java/com/example/schedulev2/controller/ScheduleController.java
+++ src/main/java/com/example/schedulev2/controller/ScheduleController.java
@@ -1,4 +1,27 @@
 package com.example.schedulev2.controller;
 
+import com.example.schedulev2.dto.CreateScheduleRequestDto;
+import com.example.schedulev2.dto.ScheduleResponseDto;
+import com.example.schedulev2.service.ScheduleService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/schedules")
+@RequiredArgsConstructor
 public class ScheduleController {
+
+    private final ScheduleService scheduleService;
+
+    @PostMapping("(/{writer_id}")
+    public ResponseEntity<ScheduleResponseDto> saveSchedule(
+            @PathVariable Long writer_id,
+            @RequestBody CreateScheduleRequestDto requestDto) {
+
+        ScheduleResponseDto savedSchedule = scheduleService.save(writer_id, requestDto.getTitle(), requestDto.getContents());
+        return new ResponseEntity<>(savedSchedule,HttpStatus.CREATED);
+    }
+
 }
