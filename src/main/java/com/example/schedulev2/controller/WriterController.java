ddf847b21b09589d10d35cfca504a97deae8b3fb
obt <byeongtaek12@naver.com>
2025-04-01 15:38:48 +0900

    feat: WriterController클래스) 컨트롤러 생성 및 메서드 추가
    컨트롤러에 맞는 조건들 어노테이션으로 추가 해주고 생성자 주입해주고 회원가입 메서드도 추가

--- src/main/java/com/example/schedulev2/controller/WriterController.java
+++ src/main/java/com/example/schedulev2/controller/WriterController.java
@@ -1,4 +1,31 @@
 package com.example.schedulev2.controller;
 
+import com.example.schedulev2.dto.SignUpRequestDto;
+import com.example.schedulev2.dto.SignUpResponseDto;
+import com.example.schedulev2.service.WriterService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/writers")
+@RequiredArgsConstructor
 public class WriterController {
+
+    private final WriterService writerService;
+
+    @PostMapping
+    public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto requestDto) {
+
+        SignUpResponseDto signUpResponseDto = writerService.signUp(requestDto.getWriter(), requestDto.getEmail(),
+                requestDto.getPassword());
+
+        return new ResponseEntity<>(signUpResponseDto,HttpStatus.CREATED);
+    }
+
+
 }
