7321d9919321de1003d106413b50062ebef28839
obt <byeongtaek12@naver.com>
2025-04-01 18:02:58 +0900

    feat: WriterServiceImpl클래스)메서드 추가
    모든 유저들 조회하는 메서드 추가

--- src/main/java/com/example/schedulev2/service/WriterServiceImpl.java
+++ src/main/java/com/example/schedulev2/service/WriterServiceImpl.java
@@ -1,12 +1,13 @@
 package com.example.schedulev2.service;
 
-import com.example.schedulev2.dto.SignUpResponseDto;
+import com.example.schedulev2.dto.WriterResponseDto;
 import com.example.schedulev2.entity.Writer;
 import com.example.schedulev2.repository.WriterRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,13 +15,20 @@ public class WriterServiceImpl implements WriterService{
     private final WriterRepository writerRepository;
 
     @Override
-    public SignUpResponseDto signUp(String writer, String email, String password) {
+    public WriterResponseDto signUp(String writer, String email, String password) {
 
         Writer writer1 = new Writer(writer, email, password);
 
         Writer savedWriter = writerRepository.save(writer1);
 
-        return new SignUpResponseDto(savedWriter.getId(),savedWriter.getWriter(), LocalDateTime.now(),
+        return new WriterResponseDto(savedWriter.getId(),savedWriter.getWriter(), LocalDateTime.now(),
                 LocalDateTime.now());
+    }
+
+    @Override
+    public List<WriterResponseDto> findAll() {
+
+        return writerRepository.findAll().stream().map(WriterResponseDto::new).toList();
+
     }
 }
