449d2bbe0ff2278d476d8b97708204b816086a35
obt <byeongtaek12@naver.com>
2025-04-02 17:07:07 +0900

    feat: WriterRepository인터페이스 default 메서드 추가
    로그인 기능을 위해서 이메일과 패스워드로 유저가 있는지 찾는 메서드 추가

--- src/main/java/com/example/schedulev2/repository/WriterRepository.java
+++ src/main/java/com/example/schedulev2/repository/WriterRepository.java
@@ -6,11 +6,21 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.Optional;
+
 @Repository
 public interface WriterRepository extends JpaRepository<Writer, Long> {
+
+    Optional<Writer> findWriterByEmailAndPassword(String email,String password);
 
     default Writer findByIdOrElseThrow(Long writer_id){
         return findById(writer_id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
                         "Does not exist writer_id: " + writer_id));
+    }
+
+    default Writer findWriterByEmailAndPasswordOrElseThrow(String email, String password) {
+        return findWriterByEmailAndPassword(email,password).orElseThrow(() -> new ResponseStatusException(
+                HttpStatus.NOT_FOUND,
+                "Does not exist writer"));
     }
 }
