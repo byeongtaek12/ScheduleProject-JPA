b6b54294d4c3c39ce44f329ebb1e82d5faea6149
obt <byeongtaek12@naver.com>
2025-04-02 17:03:24 +0900

    feat: LoginResponseDto클래스) 생성
    로그인할 때 응답을 다르게 해줘야 할 것 같아서 생성

---
+++ src/main/java/com/example/schedulev2/dto/LoginResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.schedulev2.dto;
+
+import lombok.Getter;
+
+@Getter
+public class LoginResponseDto {
+
+    private final Long id;
+
+    private final String writer;
+
+
+    public LoginResponseDto(Long id, String writer) {
+        this.id = id;
+        this.writer = writer;
+    }
+}
+
