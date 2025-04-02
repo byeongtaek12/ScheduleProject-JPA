8d5f798007895ee9ba7c2acd62cbd3bade13b559
obt <byeongtaek12@naver.com>
2025-04-02 17:31:49 +0900

    feat: LoginFilter클래스) 필터 생성
    로그인 검증 필터 생성

---
+++ src/main/java/com/example/schedulev2/filter/LoginFilter.java
@@ -0,0 +1,35 @@
+package com.example.schedulev2.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+    private static final String  White_List = "/login";
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        String requestURI = request.getRequestURI();
+
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        if (!White_List.equals(requestURI)) {
+            HttpSession httpSession = request.getSession(false);
+
+            if (httpSession==null || httpSession.getAttribute("sessionKey")==null) {
+                throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"로그인이 필요합니다");
+            }
+        }
+
+        filterChain.doFilter(request,response);
+    }
+
+}
