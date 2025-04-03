916139132244482bdd08d17913120138c0e85049
obt <byeongtaek12@naver.com>
2025-04-03 11:04:03 +0900

    feat: ScheduleService인터페이스) 메서드 추가
    모든 일정 조회 메서드 추가

--- src/main/java/com/example/schedulev2/service/ScheduleService.java
+++ src/main/java/com/example/schedulev2/service/ScheduleService.java
@@ -2,6 +2,10 @@ package com.example.schedulev2.service;
 
 import com.example.schedulev2.dto.ScheduleResponseDto;
 
+import java.util.List;
+
 public interface ScheduleService {
     ScheduleResponseDto save(Long writer_id, String title, String contents);
+
+    List<ScheduleResponseDto> findAllSchedule(Long writer_id);
 }
