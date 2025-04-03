879bea3b701a0a7b6a05cbab114e08c86721203f
obt <byeongtaek12@naver.com>
2025-04-03 11:29:00 +0900

    feat: ScheduleService인터페이스) 메서드 추가
    단건 일정 조회 메서드 추가

--- src/main/java/com/example/schedulev2/service/ScheduleService.java
+++ src/main/java/com/example/schedulev2/service/ScheduleService.java
@@ -8,4 +8,6 @@ public interface ScheduleService {
     ScheduleResponseDto save(Long writer_id, String title, String contents);
 
     List<ScheduleResponseDto> findAllSchedule(Long writer_id);
+
+    ScheduleResponseDto findScheduleById(Long writer_id, Long id);
 }
