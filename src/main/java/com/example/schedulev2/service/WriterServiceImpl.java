f54bb90d55ca115673d5395e3eec43995792d222
obt <byeongtaek12@naver.com>
2025-04-03 12:49:52 +0900

    feat: WriterServiceImpl클래스) 메서드 수정
    repository에서 default 메서드로 동작하는 부분을 제거하고 service에서 동작하도록 수정

--- src/main/java/com/example/schedulev2/service/WriterServiceImpl.java
+++ src/main/java/com/example/schedulev2/service/WriterServiceImpl.java
@@ -7,7 +7,9 @@ import com.example.schedulev2.dto.WriterUpdateResponseDto;
 import com.example.schedulev2.entity.Writer;
 import com.example.schedulev2.repository.WriterRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -30,32 +32,73 @@ public class WriterServiceImpl implements WriterService{
     @Override
     public LoginResponseDto login(String email, String password) {
 
-        Writer findWriter = writerRepository.findWriterByEmailAndPasswordOrElseThrow(email, password);
+        Writer findWriter = writerRepository.findWriterByEmailAndPassword(email,password)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.UNAUTHORIZED,
+                        "이메일이나 패스워드가 틀려서 로그인 실패했습니다."));
 
         return new LoginResponseDto(findWriter.getId(),findWriter.getWriter());
     }
 
     @Override
-    public List<WriterResponseDto> findAllWriter() {
+    public List<WriterResponseDto> findAllWriter(Long writer_id) {
+
+        checkSessionKey(writer_id);
 
         return writerRepository.findAll().stream().map(WriterResponseDto::new).toList();
 
     }
 
     @Override
-    public WriterResponseDto findWriterById(Long id) {
+    public WriterResponseDto findWriterById(Long writer_id,Long id) {
 
-        Writer findWriter = writerRepository.findByIdOrElseThrow(id);
+        checkSessionKey(writer_id);
 
-        return new WriterResponseDto(findWriter.getId(),findWriter.getWriter(),findWriter.getCreatedAt(),
-                findWriter.getModifiedAt());
+        return new WriterResponseDto(findWriter(id).getId(),findWriter(id).getWriter(),findWriter(id).getCreatedAt(),
+                findWriter(id).getModifiedAt());
+
     }
 
     @Override
-    public WriterUpdateResponseDto updateWriter(Long id, WriterRequestDto writerRequestDto) {
+    public WriterUpdateResponseDto updateWriter(Long writer_id,Long id, WriterRequestDto writerRequestDto) {
 
-        Writer findWriter = writerRepository.findByIdOrElseThrow(id);
+        checkSessionKey(writer_id);
 
+        Writer findWriter = findWriter(id);
+
+        notNullSetWriter(writerRequestDto,findWriter);
+
+        writerRepository.save(findWriter);
+
+        return new WriterUpdateResponseDto(findWriter.getId(),findWriter.getWriter(),findWriter.getEmail(),
+                findWriter.getPassword(),findWriter.getCreatedAt(),LocalDateTime.now());
+
+    }
+
+    @Override
+    public void deleteWriter(Long writer_id,Long id) {
+
+        checkSessionKey(writer_id);
+
+        writerRepository.delete(findWriter(id));
+
+    }
+
+    private void checkSessionKey(Long writer_id) {
+
+        if (writer_id==null) {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"로그인부터 해주세요");
+        }
+
+    }
+
+    private Writer findWriter(Long id) {
+
+        return writerRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
+                "Does not exist id: " + id));
+
+    }
+
+    private void notNullSetWriter(WriterRequestDto writerRequestDto, Writer findWriter) {
         if (writerRequestDto.getWriter()!=null) {
             findWriter.setWriter(writerRequestDto.getWriter());
         }
@@ -67,23 +110,5 @@ public class WriterServiceImpl implements WriterService{
         if (writerRequestDto.getPassword()!=null) {
             findWriter.setPassword(writerRequestDto.getPassword());
         }
-
-        writerRepository.save(findWriter);
-
-        return new WriterUpdateResponseDto(findWriter.getId(),findWriter.getWriter(),findWriter.getEmail(),
-                findWriter.getPassword(),findWriter.getCreatedAt(),LocalDateTime.now());
-
     }
-
-    @Override
-    public void deleteWriter(Long id) {
-
-        Writer findWriter = writerRepository.findByIdOrElseThrow(id);
-
-        writerRepository.delete(findWriter);
-    }
-
-
-
-
 }
