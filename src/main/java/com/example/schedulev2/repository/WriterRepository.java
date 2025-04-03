c4286c5cd3f7de7c0398832de8a9704482a26c7f
obt <byeongtaek12@naver.com>
2025-04-03 12:47:56 +0900

    chore: WriterRepository인터페이스) default 메서드 삭제
    repository는 db와의 로직만 수행해야 하기 때문에 에러를 던져주는 메서드를 service부분에서 해주는 게 좋다고 하여
    삭제

--- src/main/java/com/example/schedulev2/repository/WriterRepository.java
+++ src/main/java/com/example/schedulev2/repository/WriterRepository.java
@@ -2,9 +2,7 @@ package com.example.schedulev2.repository;
 
 import com.example.schedulev2.entity.Writer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.Optional;
 
@@ -13,13 +11,4 @@ public interface WriterRepository extends JpaRepository<Writer, Long> {
 
     Optional<Writer> findWriterByEmailAndPassword(String email,String password);
 
-    default Writer findByIdOrElseThrow(Long writer_id){
-        return findById(writer_id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,
-                        "Does not exist writer_id: " + writer_id));
-    }
-
-    default Writer findWriterByEmailAndPasswordOrElseThrow(String email, String password) {
-        return findWriterByEmailAndPassword(email,password).orElseThrow(() -> new ResponseStatusException(
-                HttpStatus.UNAUTHORIZED, "이메일이나 패스워드가 틀려서 로그인 실패했습니다."));
-    }
 }
