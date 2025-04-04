6c757e6472e07f802c0fb6c18fc8e06a25ca3a4d
obt <byeongtaek12@naver.com>
2025-04-04 11:30:25 +0900

    style: PasswordEncorder클래스) 주석 추가
    ``

--- src/main/java/com/example/schedulev2/config/PasswordEncoder.java
+++ src/main/java/com/example/schedulev2/config/PasswordEncoder.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class PasswordEncoder {
 
+
+    // 패스워드 암호화 해주는 메서드
     public String encode(String rawPassword) {
         return BCrypt.withDefaults().hashToString(BCrypt.MIN_COST, rawPassword.toCharArray());
     }
 
+    // 패스워드와 암호화된 패스워드 비교해주는 메서드
     public boolean matches(String rawPassword, String encodedPassword) {
         BCrypt.Result result = BCrypt.verifyer().verify(rawPassword.toCharArray(), encodedPassword);
         return result.verified;
